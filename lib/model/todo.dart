3fbd33c75b0cc5974fbf4222e53e6343c17d1cae
Nosrey <nosrey135@gmail.com>
2024-08-06 22:14:02 -0400

    avances en las listas todo
    las listas ahora estan construidas, el textInput ya esta agregado, las listas ahora ya tienen la logica para adaptar su visual en base a si estan activadas o no

---
+++ lib/model/todo.dart
@@ -0,0 +1,21 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', todoText: 'Programar en Flutter', isDone: true),
+      ToDo(id: '02', todoText: 'Hacer ejercicio', isDone: true),
+      ToDo(id: '03', todoText: 'Leer un libro', isDone: false),
+      ToDo(id: '04', todoText: 'Aprender algo nuevo', isDone: false),
+      ToDo(id: '05', todoText: 'Ver una película', isDone: false),
+    ];
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,38 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list_flutter/constants/colors.dart';
+import '../constants/colors.dart';
+import '../widgets/todo_item.dart';
+import '../model/todo.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  final todosList = ToDo.todoList();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10),
-                  prefixIcon: Padding(
-                      padding: EdgeInsets.only(right: 10),
-                      child: Icon(Icons.search, color: tdBlack, size: 20)),
-                  prefixIconConstraints:
-                      BoxConstraints(maxHeight: 20, minWidth: 25),
-                  border: InputBorder.none,
-                  hintText: 'Search',
-                  hintStyle: TextStyle(color: tdGrey),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                SearchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        child: Text(
+                          'All ToDos',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      for (ToDo todoo in todosList) TodoItem(todo: todoo),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
+              ],
+            ),
+          ),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 10.0,
+                            spreadRadius: 0.0,
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Add a new todo item',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ))
+        ],
       ),
     );
   }
@@ -50,6 +89,33 @@ class Home extends StatelessWidget {
                 borderRadius: BorderRadius.circular(20),
                 child: Image.asset('assets/images/avatar.jpg')))
       ]),
+    );
+  }
+}
+
+class SearchBox extends StatelessWidget {
+  const SearchBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(10),
+          prefixIcon: Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: Icon(Icons.search, color: tdBlack, size: 20)),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
+      ),
     );
   }
 }
